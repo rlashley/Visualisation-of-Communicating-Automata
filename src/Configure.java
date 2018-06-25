9e6d4a579516f41c20be45ab4a46634a67ff4939
rlashley <rlashley@DESKTOP-2QK2IKA.home>
2018-06-25 18:32:41 +0100

    A few bug fixes

--- src/Configure.java
+++ src/Configure.java
@@ -49,7 +49,6 @@ public class Configure {
 		String nextLine = null;
 		ArrayList<String> listOfCurrentTransitions = new ArrayList<String>();
 		int nextInt = 0;
-		int count = 0;
 		
 
 		while(!(exit.toLowerCase().equals("yes"))) {
@@ -78,15 +77,14 @@ public class Configure {
 	        	String[] apart = nextLine.split(" ");
 	        	String queueName = Integer.toString(nextInt)+apart[1];
 	        	queue.get(queueName).add(nextLine);
-		        count++;
 		        nextInt++;
 	        }
 	        else {
 	        	System.out.println("Your choice is not in the list of possible transactions.");
 	        }
 	        //Counts up to a single queue selection for each machine before automatically firing transitions.
-	        if(count == machines.size()) {
-	        	String[] swap;
+	        if(nextInt == machines.size()) {
+	        	String[] swap = null;
 	        	String[] apart = null, apart2 = null;
 	        	String compare, compare2;
 	        	
@@ -94,51 +92,45 @@ public class Configure {
 	        	for(String key : queue.keySet()) {
 	        		swap = key.split("");
 	        		String key2 = swap[1]+swap[0];
-	        		if(!queue.get(key).isEmpty()) {
+	        		if((!queue.get(key).isEmpty()) && (!queue.get(key2).isEmpty())) {
 	        			compare = queue.get(key).getFirst();
 		        		apart = compare.split(" ");
-		        	}
-	        		if(!queue.get(key2).isEmpty()) {
-	        		compare2 = queue.get(key2).getFirst();
-	        		apart2 = compare2.split(" ");
-	        		}
-	        		if(apart[1].equals(swap[1]) && apart2[1].equals(swap[0])) {
-	        			if( (apart[2].equals("!") && apart2[2].equals("?")) ) {
-	        				if(apart[3].equals(apart2[3])) {
-	        					System.out.println("Message from machine " + swap[0] + " has been delivered to machine " + swap[1]);
-	        					//Assign apart[3] to swap[1]
-	        					machines.get(Integer.parseInt(swap[1])).saveMessages(apart[3]);
-	        					//Update states of machines
-	        					states.put(swap[0], apart[4]);
-	        					states.put(swap[1], apart2[4]);
-	        					//Removes from the queue the successful transitions.
-	        					queue.get(key).remove();
-	        					queue.get(key2).remove();
+		        		compare2 = queue.get(key2).getFirst();
+		        		apart2 = compare2.split(" ");
+	        		
+		        		if(apart[1].equals(swap[1]) && apart2[1].equals(swap[0])) {
+		        			if( (apart[2].equals("!") && apart2[2].equals("?")) ) {
+		        				if(apart[3].equals(apart2[3])) {
+		        					//Assign message to proper machine
+		        					machines.get(Integer.parseInt(swap[1])).saveMessages(apart[3]);
+		        					System.out.println("Message from machine " + swap[0] + " has been delivered to machine " + swap[1]);
+		        					//Update states of machines
+		        					states.put(swap[0], apart[4]);
+		        					states.put(swap[1], apart2[4]);
+		        					//Removes from the queue the successful transitions.
+		        					queue.get(key).remove();
+		        					queue.get(key2).remove();
 
-	        				}
-	        				
-	        				else {
-	        					System.out.println("Messages in queue did not match");
-	        				}
-	        				
-	        			}	
-	        			else {
-	        				System.out.println("Send/Receive states did not match");
-	        			}
-	        			
+		        				}		        				
+		        				else {
+		        					System.out.println("Messages in queue did not match");
+		        				}	        				
+		        			}	
+		        			else {
+		        				System.out.println("Send/Receive states did not match");
+		        			}
+		        		}
 	        		}
 	        		
 	        	}
 		        for(String key : queue.keySet()) {
 		        	if(!queue.get(key).isEmpty()) {
-		        		System.out.println("Queue " + key + " is not empty. It has the following transitions " + queue.get(key).toString());
+		        		System.out.println("Queue for machine " + key.charAt(0) + " is not empty. The following transitions can't occur. " + queue.get(key).toString());
 		        	}
 		        }
-		        nextInt = 0;
-		        count = 0;    
+		        nextInt = 0;  
 	        }
 	        listOfCurrentTransitions.clear();
-
 		}
 	}  
 }
